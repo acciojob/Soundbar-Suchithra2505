eacfa38884da9324a7360bbc64beccbf9586367d
Suchithra2505 <147540982+Suchithra2505@users.noreply.github.com>
2023-12-26 14:36:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let audio;
+
+function playAudio(filename) {
+  stopAudio(); // Stop any currently playing audio before starting a new one
+  audio = new Audio(`sounds/${filename}`);
+  audio.play();
+}
+
+function stopAudio() {
+  if (audio) {
+    audio.pause();
+    audio.currentTime = 0;
+  }
+}
